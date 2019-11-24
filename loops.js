b406a2818d890bcd3e87fd996d688ac0a493ae9c
gabmadera <gabmadera@gmail.com>
2019-11-24 22:05:37 +0100

    sigo con problemas

--- loops.js
+++ loops.js
@@ -1,12 +1,17 @@
 var n = Math.floor((Math.random() * 100) + 1);
-var par = [];
-var impar = [];
-for (var i = 0; i <= 50; i++) {
-  if (n%2==0) {
-    par.push(n);
-  } else {
-    impar.push(n);
-  };
+var impar=[], par=[];
+function f(){
+  for (var i= 0; i< 51; i++) {
+    if (n%2==0) {
+      par.push(n);
+    }
+
+    else {
+      impar.push(n);
+    }
+
+  }
+
+  return document.write(par), alert(impar);
 };
-document.write(par);
-alert(impar);
+f(n);
